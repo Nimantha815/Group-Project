2db75a635c5494e6c4b9d46c54c5e0e0f1e965cc
Nimantha815 <nimanthaaravinda19970815@gmail.com>
2020-12-17 11:01:33 +0530

    about us details

---
+++ About Us/aboutus.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>
+        About Us
+    </title>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Handloom,Handicrafts">
+    <meta name="description" content="Online shopping platform">
+    <meta http-equiv="refresh" content="300">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="..\css\header.css">
+    <link rel="stylesheet" href="..\css\footer.css">
+    <link rel="stylesheet" href="aboutus.css">
+</head>
+
+<body>
+    <?php include('../php/header1.php'); ?>
+
+    <div id="accordion" class="panel-group">
+        <div class="panel panel-info" style="cursor: pointer;">
+            <div class="panel-heading" style="background-color: #fff;" data-toggle="collapse" data-parent="#accordion" data-target="#collapse1">
+                <h2 class="panel-title accordion-toggle" style="color: #2196f3; font-size: 18px; margin: 10px 0px 10px 0px;">What safety measures has Daraz taken to make sure packages are not infected?</h2>
+            </div>
+            <div id="collapse1" class="panel-collapse collapse">
+                <div class="panel-body" style="background-color: #f1f1f1;">
+                    <p>Delivery agents and operations team have been instructed to wear protective gear, including gloves and masks. Our Logistics Partners have been given the same instructions. We are also making sure that packages are being disinfected
+                        at customers' doorstep wherever possible.</p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-info" style="cursor: pointer;">
+            <div class="panel-heading" style="background-color: #fff;" data-toggle="collapse" data-parent="#accordion" data-target="#collapse2">
+                <h2 class="panel-title accordion-toggle" style="color: #2196f3; font-size: 18px; margin: 10px 0px 10px 0px;">Can the COVID-19 virus be transmitted via packages?</h2>
+            </div>
+            <div id="collapse2" class="panel-collapse collapse">
+                <div class="panel-body" style="background-color: #f1f1f1;">
+                    <p>There is no evidence indicating packages or their contents carry any risks. We are monitoring the situation and will cooperate with relevant authorities.</p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-info" style="cursor: pointer;">
+            <div class="panel-heading" style="background-color: #fff;" data-toggle="collapse" data-parent="#accordion" data-target="#collapse3">
+                <h2 class="panel-title accordion-toggle" style="color: #2196f3; font-size: 18px; margin: 10px 0px 10px 0px;">How can I track my order?</h2>
+            </div>
+            <div id="collapse3" class="panel-collapse collapse">
+                <div class="panel-body" style="background-color: #f1f1f1;">
+                    <p>There is option in our site to track your orders & find the current state of order</p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-info" style="cursor: pointer;">
+            <div class="panel-heading" style="background-color: #fff;" data-toggle="collapse" data-parent="#accordion" data-target="#collapse4">
+                <h2 class="panel-title accordion-toggle" style="color: #2196f3; font-size: 18px; margin: 10px 0px 10px 0px;">How do you change the Contact number?</h2>
+            </div>
+            <div id="collapse4" class="panel-collapse collapse">
+                <div class="panel-body" style="background-color: #f1f1f1;">
+                    <p>Login to your Account and click on update account and you can change your mobike number or other Informations</p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-info" style="cursor: pointer;">
+            <div class="panel-heading" style="background-color: #fff;" data-toggle="collapse" data-parent="#accordion" data-target="#collapse5">
+                <h2 class="panel-title accordion-toggle" style="color: #2196f3; font-size: 18px; margin: 10px 0px 10px 0px;">Is it safe to pay with credit / debit card?</h2>
+            </div>
+            <div id="collapse5" class="panel-collapse collapse">
+                <div class="panel-body" style="background-color: #f1f1f1;">
+                    <p>Fraud detection and prevention are very important to us. We take all steps to ensure that transactions are genuine and that our customers’ details are completely secure.</p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-info" style="cursor: pointer;">
+            <div class="panel-heading" style="background-color: #fff;" data-toggle="collapse" data-parent="#accordion" data-target="#collapse6">
+                <h2 class="panel-title accordion-toggle" style="color: #2196f3; font-size: 18px; margin: 10px 0px 10px 0px;">Does <strong>Helakam.lk</strong> deliver across all of Sri Lanka?</h2>
+            </div>
+            <div id="collapse6" class="panel-collapse collapse">
+                <div class="panel-body" style="background-color: #f1f1f1;">
+                    <p><strong>Helakam.lk</strong> delivers all across Sri Lanka through its logistics partners.</p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-info" style="cursor: pointer;">
+            <div class="panel-heading" style="background-color: #fff;" data-toggle="collapse" data-parent="#accordion" data-target="#collapse7">
+                <h2 class="panel-title accordion-toggle" style="color: #2196f3; font-size: 18px; margin: 10px 0px 10px 0px;">What are the shipping charges?</h2>
+            </div>
+            <div id="collapse7" class="panel-collapse collapse">
+                <div class="panel-body" style="background-color: #f1f1f1;">
+                    <ul>
+                        <li>Shipping rate is calculated is based on the weight of the products, the origin from where the item is picked from and the destination to where it is delivered.</li>
+                    </ul>
+                    <ul>
+                        <li>The total shipping fee charged will be clearly indicated for each product individually at the main product page and the total shipping fee for the cart will be displayed at the checkout page.</li>
+                    </ul>
+                    <ul>
+                        <li>The shipping charges are always shown separately from the price of the product.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include('footer.php');?> 
+</body>
+
+</html>
---
+++ About Us/header.php
@@ -0,0 +1,41 @@
+<button class="sc" onclick="topFunction()" id="myBtn" title="Go to top">^</button>
+<div class="container">
+        <dev class="part search">   
+             <input id="searchBar" class="searchbar" type="text" placeholder="Search in...">
+      <a id="btnSearch" class="btn-search"><i class="fa fa-search" aria-hidden="true"></i></a>
+        </dev>
+
+        <div class="part logo"> 
+            <img src=" ////" alt="">
+        </div>
+
+        <div class="part headericon">   
+            <i class="fa fa-user-circle" aria-hidden="true"></i>
+            <i class="fa fa-heart" aria-hidden="true"></i>
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+        </div>
+ 
+    </div>
+    <hr>
+<header>
+      <div id="nav">
+           <ul>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="servicepage.php">Services</a></li>
+        <li><a href="">Category</a>
+            <ul class="dropdown">
+                <li><a href="catogery\cat1.php">Home & Living</a></li>
+                <li><a href="catogery\cat2.php">Arts & Crafts</a></li>
+                <li><a href="catogery\cat3.php">Tools & Home Improvement</a></li>
+                <li><a href="catogery\cat4.php">Home Gardening</a></li>
+                <li><a href="catogery\cat5.php">Toys & Educational</a></li>
+                <li><a href="catogery\cat6.php">Kitchen & Dining</a></li>
+            </ul>
+        </li>
+        <li><a href="">About us</a></li>
+       
+       
+    </ul>  
+      </div>
+    </header>
+    <hr>
