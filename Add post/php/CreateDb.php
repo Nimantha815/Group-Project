98d49fa5877e9f1be6524f2eb0ac4135ce188ab3
Nimantha815 <nimanthaaravinda19970815@gmail.com>
2020-12-18 22:36:24 +0530

    Php file database

---
+++ Add post/php/CreateDb.php
@@ -0,0 +1,77 @@
+<?php
+
+
+class CreateDb
+{
+        public $servername;
+        public $username;
+        public $password;
+        public $dbname;
+        public $tablename;
+        public $con;
+
+
+        // class constructor
+    public function __construct(
+        $dbname = "Newdb",
+        $tablename = "Productdb",
+        $servername = "localhost",
+        $username = "root",
+        $password = ""
+    )
+    {
+      $this->dbname = $dbname;
+      $this->tablename = $tablename;
+      $this->servername = $servername;
+      $this->username = $username;
+      $this->password = $password;
+
+      // create connection
+        $this->con = mysqli_connect($servername, $username, $password);
+
+        // Check connection
+        if (!$this->con){
+            die("Connection failed : " . mysqli_connect_error());
+        }
+
+        // query
+        $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+
+        // execute query
+        if(mysqli_query($this->con, $sql)){
+
+            $this->con = mysqli_connect($servername, $username, $password, $dbname);
+
+            // sql to create new table
+            $sql = " CREATE TABLE IF NOT EXISTS $tablename
+                            (post_id int(11) NOT NULL,
+                            description varchar(255) NOT NULL,
+                             imageName varchar(500) DEFAULT NULL
+                            );";
+
+            if (!mysqli_query($this->con, $sql)){
+                echo "Error creating table : " . mysqli_error($this->con);
+            }
+
+        }else{
+            return false;
+        }
+    }
+
+    // get product from the database
+    public function getData(){
+        $sql = "SELECT * FROM $this->tablename";
+
+        $result = mysqli_query($this->con, $sql);
+
+        if(mysqli_num_rows($result) > 0){
+            return $result;
+        }
+    }
+}
+
+
+
+
+
+
