32b9b15e2077f37750334b9b990d5db859343a20
Nimantha815 <nimanthaaravinda19970815@gmail.com>
2020-12-22 11:45:15 +0530

    Order track form details

---
+++ Order track/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="..\css\header.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="..\css\footer.css">
+	<title>Order Track</title>
+</head>
+
+
+<body>	
+  		<?php include('../php/header1.php'); ?>	
+  	<div class="post">	
+<p class="sign" align="center">Order Tracking Details</p>
+<form action="index.php" method="post" enctype="multipart/form-data" >
+<div class="remember" >
+<label "remember">Order ID</label>  
+</div>
+ <input class="un " align="center" type="text" placeholder="Order ID" name="Pname"  required>    
+ </div>
+	
+	
+</div>
+</form>
+<br>
+<?php include('../php/footer.php'); ?>
+
+  
+</body>
+</html>
+
+
+
