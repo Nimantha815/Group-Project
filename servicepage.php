0d1d4aa8bfd5d96e1ca32e233001b666a69b3564
Nimantha815 <nimanthaaravinda19970815@gmail.com>
2020-12-24 13:08:57 +0530

    Servicepage php file

---
+++ servicepage.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/servicepage.css">
+        <title>detail page</title>
+      
+    </head>
+
+    <body>
+       
+
+<header>
+
+  <div class="container">
+    <section class="hero">
+      
+    </section>
+  </div>
+  
+  <div class="container">
+    <section class="projects">
+      <div class="project-cards">
+
+        <div class="p-card">
+          <div class="card_img landing_img7"></div>
+          <div class="pcard_info">
+           
+            <strong class="card-title">
+              
+                 Your Account
+              </span>
+            
+             
+            </strong>
+          </div>
+        </div>
+
+        <div class="p-card">
+          <div class="card_img landing_img8"></div>
+          <div class="pcard_info">
+           
+            <strong class="card-title">
+              <a href="Order track/index.php"><span class="project-name">
+              Your Order
+              </span>
+              </a>
+             
+            </strong>
+          </div>
+        </div>
+
+        <div class="p-card">
+          <div class="card_img landing_img9"></div>
+          <div class="pcard_info">
+           
+            <strong class="card-title">
+              <span class="project-name">
+                Payments
+              </span>
+             
+            </strong>
+          </div>
+        </div>
+
+        <div class="p-card">
+          <div class="card_img landing_img1"></div>
+          <div class="pcard_info">
+           
+            <strong class="card-title">
+              <a href="Login/loginA.php"><span class="project-name">
+              <span class="project-name">
+                Edit website
+              </span>
+            </a>
+             
+            </strong>
+          </div>
+        </div>
+        <div class="p-card">
+          <div class="card_img landing_img10"></div>
+          <div class="pcard_info">
+           
+            <strong class="card-title">
+              <a href="Login/loginD.php"><span class="project-name">
+                Delivery Services
+              </span>
+              </a>
+             
+            </strong>
+          </div>
+        </div>
+ 
+        
+      </div>
+    </section>
+  </div>
+ 
+
+ 
+</header>
+
+
+    </body>
+    
+</html> 
